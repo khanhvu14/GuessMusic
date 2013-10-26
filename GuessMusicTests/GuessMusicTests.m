8e9d72a984727baecb75e4f4d55ef1739ab517e4
khanh_vu14 <khanh_vu14@live.com>
2013-10-26 12:23:19 +0700

    Create new project

---
+++ GuessMusicTests/GuessMusicTests.m
@@ -0,0 +1,34 @@
+//
+//  GuessMusicTests.m
+//  GuessMusicTests
+//
+//  Created by April Coder on 10/26/13.
+//  Copyright (c) 2013 April Coder. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GuessMusicTests : XCTestCase
+
+@end
+
+@implementation GuessMusicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
